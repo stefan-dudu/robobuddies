8d4fcbb836baac20016e9c67e6fb9afc69bfe195
stefan01-dev <stefan01-dev@users.noreply.github.com>
2020-10-20 20:47:16 +0300

    adding the main files

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+	return(
+		<div>
+			<img  alt = 'alt text' src= 'https://robohash.org/ool?500x500' />
+			<div> 
+				<h2> here is the title unde the photo </h2>
+				<p> here is the email </p>
+			</div>		
+		</div>
+		)
+}
+ 
+export default Card;
