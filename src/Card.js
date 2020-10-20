38fb4c02d5aa099a2a41870496d2b394dee373c3
stefan01-dev <stefan01-dev@users.noreply.github.com>
2020-10-20 22:45:23 +0300

    enought for tonight

--- src/Card.js
+++ src/Card.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Card = ({id, name, email}) => {
 	
 	return(
-		<div className = 'tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img  alt = 'alt text' src= {`https://robohash.org/${id}?200x200`} />
+		<div className = 'tc bg-light-green dib br3 pa2 ma2 grow bw2 shadow-5'>
+			<img  alt = 'alt text' src= {`https://robohash.org/${id}?size=100x100`} />
 			<div> 
 				<h2> Name: {name} </h2>
 				<p> {email} </p>
