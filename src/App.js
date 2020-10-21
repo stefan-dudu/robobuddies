7d3bee20183002825c74592cc2ad60144601afaa
Stefan Dudu <dudu.steffan@yahoo.com>
2020-10-21 23:51:34 +0300

    part3 done

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
 import {robots} from './robots';
 
 class App extends Component {
-		constructor() {
+	constructor() {
 		super()
 		this.state = {
 			robots: robots,
 			searchfield: ''
 		}
 	}
-	render () {
+
+	onSearchChange = (event) => {
+		this.setState({searchfield: event.target.value })
+	}
+
+	render() {
+		const filteredRobots = this.state.robots.filter(robots =>{
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+		})
 		return (
 		  <div className='tc'>
 			<h1> RoboBuddies </h1>
-			<SearchBox />
-			<CardList robots = {robots} />
+			<SearchBox searchChange={this.onSearchChange}/>
+			<CardList robots = {filteredRobots} />
 		 </div>
 		)
 	}
