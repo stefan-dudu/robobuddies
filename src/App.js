fcc0536957479391cf82714c6a14e144d182d902
stefan01-dev <stefan01-dev@users.noreply.github.com>
2020-10-21 22:55:11 +0300

    partial update

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox'
+import {robots} from './robots';
+
+class App extends Component {
+		constructor() {
+		super()
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
+	render () {
+		return (
+		  <div className='tc'>
+			<h1> RoboBuddies </h1>
+			<SearchBox />
+			<CardList robots = {robots} />
+		 </div>
+		)
+	}
+}
+
+export default App;
