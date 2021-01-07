8e87d5e9c24350e99506ff468cb47d8fede65feb
stefan01-dev <stefan01.dev@gmail.com>
2021-01-07 06:45:33 +0200

    Updates

---
+++ precache-manifest.0f2c8d13de776511b67b2a84dac217d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8791254846a7ca9cf57ab413c360efad",
+    "url": "/robobuddies/index.html"
+  },
+  {
+    "revision": "737280082adbdcc18137",
+    "url": "/robobuddies/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "84c951e88d72caedfb77",
+    "url": "/robobuddies/static/css/main.73bc5da6.chunk.css"
+  },
+  {
+    "revision": "737280082adbdcc18137",
+    "url": "/robobuddies/static/js/2.e1d86dab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robobuddies/static/js/2.e1d86dab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c951e88d72caedfb77",
+    "url": "/robobuddies/static/js/main.01fc140f.chunk.js"
+  },
+  {
+    "revision": "87f9d18695c76aa664fe",
+    "url": "/robobuddies/static/js/runtime-main.661643b6.js"
+  },
+  {
+    "revision": "831ef4d9bd2efd695bb1f008353ca244",
+    "url": "/robobuddies/static/media/IMPOS50_.831ef4d9.woff"
+  }
+]);
