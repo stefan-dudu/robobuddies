5986a274c8c83cbdae16552cedf9c126d58b3590
stefan01-dev <stefan01.dev@gmail.com>
2021-03-30 06:25:30 +0300

    useState - hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from 'react';
+import React, {useState, useEffect } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
 import {robots} from './robots';
@@ -6,9 +6,9 @@ import './app.css'
 
 
 
-function App(){
+function App1(){
 	const [robots, setRobots] = useState(robots);
-	const [searchfield, setSearchfield ] = setState(searchfield);
+	const [searchfield, setSearchfield ] = useState(searchfield);
 }
 
 
@@ -18,37 +18,40 @@ return (
 	})
 )
 
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			robots: robots,
-			searchfield: ''
-		}
-	}
+// class App extends Component {
+// 	constructor() {
+// 		super()
+// 		this.state = {
+// 			robots: robots,
+// 			searchfield: ''
+// 		}
+// 	}
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users => {this.setState({robots:users})});
-	}
+// 	componentDidMount() {
+// 		fetch('https://jsonplaceholder.typicode.com/users')
+// 		.then(response => response.json())
+// 		.then(users => {this.setState({robots:users})});
+// 	}
+function App(){
 
-	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value })
-	}
+	const [robots, setRobots] = useState(robots);
+	const [searchfield, setSearchfield ] = useState('');
 
-	render() {
-		const filteredRobots = this.state.robots.filter(robots =>{
-			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+
+	const  onSearchChange = (event) => {
+		setSearchfield(event.target.value)
+	} 
+		const filteredRobots = robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
 		})
 		return (
 		  <div className='tc'>
 			<h1 className='f1'> RoboBuddies </h1>
-			<SearchBox searchChange={this.onSearchChange}/>
+			<SearchBox searchChange={onSearchChange}/>
 			<CardList robots = {filteredRobots} />
 		 </div>
 		)
-	}
 }
+
 
 export default App;
